a169273a2baaef869cc7ccbb79d7ff473f531753
Antonio_V <antoniovillano91@hotmail.it>
2023-11-22 21:04:24 +0100

    mod palindroma

--- palind_script.js
+++ palind_script.js
@@ -1,13 +1,17 @@
 let palindromo = prompt("inserisci frase da verificare:")
 function pal(str){
+    let palindroma=false
     let norm = str.replace(/\W/g, "").toLowerCase();
     let rev = norm.split("").reverse().join("")
-    return rev
+    if (norm=rev){
+        palindroma=true
+    }
+    return palindroma
 }
 let palindromo_rev= pal(palindromo)
 
 
-if (palindromo_rev==palindromo){
+if (palindromo_rev){
     console.log("la frase "+palindromo+" è palindroma")
 }
 else{
